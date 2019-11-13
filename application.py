2c9ef44bbd0ce75e1e48cf9523be844597f104ec
Hurly119 <hurlycabalan@yahoo.com>
2019-11-13 14:01:39 +0800

    Before edit

--- application.py
+++ application.py
@@ -4,6 +4,9 @@ from flask import Flask, session, render_template, request, flash, url_for, redi
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+import requests
+import xmltodict
+import json
 
 app = Flask(__name__)
 
@@ -16,6 +19,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+key = "jKHr5blcnvzAgZ9LsNUrlg"
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
@@ -23,60 +27,105 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    LoggedIn = "username" in session
+    return render_template("index.html",LoggedIn=LoggedIn)
 
-@app.route("/loginpg")
-def loginpg():
-    return render_template("login.html")
-
-@app.route("/registerpg")
-def registerpg():
-    return render_template("register.html")
-
-@app.route("/searchpg")
-def searchpg():
-    return render_template("search.html")
-
-
-@app.route("/register", methods=["POST"])
+@app.route("/register", methods=["POST", "GET"])
 def register():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    cpass = request.form.get("cpass")
-    if(username and password and cpass):
-        if db.execute("SELECT * FROM users WHERE username = :username",
-        {"username":username}).fetchone() == None:
-            if password == cpass:
-                db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
-                {"username":username, "password":password})
-                db.commit()
-                flash("Register successful")
-                return redirect(url_for('registerpg'))
-            flash("Password does not match")
-            return redirect(url_for('registerpg'))
-    flash("Account name already taken; Fields not filled.")
-    return redirect(url_for('registerpg'))
+    LoggedIn = "username" in session
+    if request.method=="POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        cpass = request.form.get("cpass")
+        if(username and password and cpass):
+            if db.execute("SELECT * FROM users WHERE username = :username",
+            {"username":username}).fetchone() == None:
+                if password == cpass:
+                    db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
+                    {"username":username, "password":password})
+                    db.commit()
+                    flash("Register successful")
+                    return redirect(url_for('register'))
+                flash("Password does not match")
+                return redirect(url_for('register'))
+            flash("Account name already Taken")
+            return redirect(url_for('register'))
+        flash("Fields not filled.")
+        return redirect(url_for('register'))
+    return render_template("register.html", LoggedIn=LoggedIn)
 
-@app.route("/login", methods=["POST","GET"])
+
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    if db.execute("SELECT * FROM users WHERE password = :password AND username = :username",
-    {"password":password, "username":username}).fetchone() == None:
-        flash("Log-in failed, wrong username or password")
-        return redirect(url_for('loginpg'))
-    flash("Log-in successful")
-    return redirect(url_for('index'))
+    LoggedIn = "username" in session
+    if request.method=="POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if db.execute("SELECT * FROM users WHERE password = :password AND username = :username",
+        {"password":password, "username":username}).fetchone() == None:
+            flash("Log-in failed, wrong username or password")
+            return redirect(url_for('login'))
+        flash("Log-in successful")
+        session["username"] = username
+        return redirect(url_for('index'))
+    return render_template("login.html", LoggedIn=LoggedIn)
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    flash("Logged-out")
+    return redirect(url_for('login'))
 
 
-@app.route("/search" , methods=["POST"])
+@app.route("/search" , methods=["POST", "GET"])
 def search():
-    ISBN = "%" + request.form.get("ISBN") +"%"
-    Title = "%" +request.form.get("TITLE")+"%"
-    Author = "%" +request.form.get("AUTHOR")+"%"
-    bookQ = db.execute("SELECT * FROM books WHERE isbn LIKE :isbn AND title LIKE :Title AND author LIKE :Author",
-    {"isbn":ISBN, "Title":Title, "Author":Author}).fetchall()
-    if bookQ == None:
-        flash("No books found")
-        return redirect(url_for('searchpg'))
-    return render_template("search.html", bookQ=bookQ)
+    LoggedIn = "username" in session
+    if request.method == "POST":
+        ISBN = "%" + request.form.get("ISBN") +"%"
+        Title = "%" +request.form.get("TITLE")+"%"
+        Author = "%" +request.form.get("AUTHOR")+"%"
+        bookQuery = db.execute("SELECT * FROM books WHERE isbn LIKE :isbn AND title LIKE :Title AND author LIKE :Author",
+        {"isbn":ISBN, "Title":Title, "Author":Author}).fetchall()
+        if not bookQuery:
+            flash("No books found")
+            return redirect(url_for('search'))
+        return render_template("search.html", bookQ=bookQuery, LoggedIn=LoggedIn)
+    return render_template("search.html", LoggedIn=LoggedIn)
+
+@app.route("/booksearch/<string:bookISBN>/<string:bookTitle>", methods=["POST","GET"])
+def booksearch(bookISBN,bookTitle):
+    LoggedIn = "username" in session
+    Reviews = db.execute("SELECT * FROM reviews").fetchall()
+    data = requests.get("https://www.goodreads.com/search/index.xml", params={"key": "jKHr5blcnvzAgZ9LsNUrlg", "q": bookISBN})
+    jsonData = json.loads(json.dumps(xmltodict.parse(data.text)))
+    try:
+        ReviewYear = jsonData["GoodreadsResponse"]["search"]["results"]["work"][0]
+    except:
+        ReviewYear = jsonData["GoodreadsResponse"]["search"]["results"]["work"]
+    BookInfo = ReviewYear['best_book']
+    PubYear = ReviewYear["original_publication_year"]["#text"]
+    RatingsCount = int(ReviewYear["ratings_count"]["#text"])
+    AVGRating = float(ReviewYear["average_rating"])
+    Author = BookInfo["author"]["name"]
+    Title = BookInfo["title"]
+    BookIMG = BookInfo["image_url"]
+    return render_template("booksearched.html",BookIMG=BookIMG, Title=Title, Author=Author,
+    AVGRating=AVGRating, RatingsCount=RatingsCount, PubYear=PubYear,LoggedIn=LoggedIn, bookISBN=bookISBN, bookTitle=bookTitle,Reviews=Reviews)
+
+@app.route("/postreview/<string:bookISBN>/<string:bookTitle>", methods=["POST"])
+def postreview(bookISBN, bookTitle):
+    LoggedIn = "username" in session
+    if LoggedIn:
+        review = request.form.get("review")
+        username = session["username"]
+        hasComment = db.execute("SELECT * FROM reviews WHERE username=:username AND isbn=:isbn AND title=:title",
+        {"username":username, "isbn":bookISBN, "title":bookTitle}).fetchall()
+        if not hasComment:
+            db.execute("INSERT INTO reviews (username, review, isbn, title) VALUES (:username, :review, :isbn, :title)",
+            {"username":username, "review":review, "isbn":bookISBN, "title":bookTitle})
+            db.commit()
+            return redirect(url_for("booksearch",bookISBN=bookISBN,bookTitle=bookTitle))
+        flash("Can't put more than one review")
+        return redirect(url_for("booksearch",bookISBN=bookISBN,bookTitle=bookTitle))
+    flash("Please log-in to review")
+    return redirect(url_for("search"))
